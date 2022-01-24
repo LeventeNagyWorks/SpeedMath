490ea43de076ccbd3a104b5f114ff81a3c14383d
Kensei-Sama <52254166+Kensei-Sama@users.noreply.github.com>
2022-01-24 14:01:40 +0100

    exit_button

--- app/src/main/java/com/example/green_math/MainActivity.java
+++ app/src/main/java/com/example/green_math/MainActivity.java
@@ -4,11 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.View;
+
+import android.view.View.OnClickListener;
+
+import android.widget.Button;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                finish();
+                System.exit(0);
+            }
+        });
     }
 }
